7a63b354ddf8de170c5e103280af41f8d137f60c
codefromalishba <alishba3939@gmail.com>
2024-09-24 12:02:57 +0500

    DropDown added

--- src/app/components/MainDashboard.js
+++ src/app/components/MainDashboard.js
@@ -32,6 +32,7 @@ const MainDashboard = () => {
                   <div className="flex items-center gap-3">
                     <Image
                       src={item.img}
+                      alt="dropDown img"
                       className="w-5 h-5 opacity-50 group-hover:opacity-100"
                     />
                     <p className="text-sm">{item.name}</p>
