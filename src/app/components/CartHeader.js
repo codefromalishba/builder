ba06ecacf2e20efa5e8fa7264284f302d600d173
codefromalishba <alishba3939@gmail.com>
2025-02-04 16:31:51 +0500

    cart header updated

--- src/app/components/CartHeader.js
+++ src/app/components/CartHeader.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CartHeader = () => {
+  const { allFeatures } = useSelector((state) => state.feature);
   return (
     <div className="flex items-center px-5 py-3 gap-2">
-      <p className="text-xl">Selected Feature</p>
-      <p className="text-xl">1</p>
+      <p className="text-xl">
+        {allFeatures?.length === 1 ? "Selected Feature" : "Selected Features"}
+      </p>
+      <p className="text-xl">{allFeatures?.length}</p>
     </div>
   );
 };
